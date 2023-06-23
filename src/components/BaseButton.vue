c65cec778ab61fd90892b3eb6d9daa8702afb1fc
umid <umid.ruzi@gmail.com>
2023-06-23 12:56:17 +0500

    component module initialization script

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -5,10 +5,15 @@
     <slot></slot>
   </button>
 </template>
-<script setup>
+<script>
 import { BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '../constants'
 import { isButtonTypeValid } from '../validators'
-
+const typeClasses = {
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200'
+}
+</script>
+<script setup>
 defineProps({
   type: {
     default: 'neutral',
@@ -16,8 +21,4 @@ defineProps({
     validator: isButtonTypeValid
   }
 })
-const typeClasses = {
-  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
-  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200'
-}
 </script>
