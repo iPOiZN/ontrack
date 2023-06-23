72ec23e4f0551ea38e221f49122fa3e9cc47ede5
umid <umid.ruzi@gmail.com>
2023-06-23 12:50:39 +0500

    button types

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,39 +1,40 @@
 <template>
-    <li class="flex flex-col gap-2 p-4">
-        <div class="flex items-center gap-2">
-            <BaseButton>
-                <TrashIcon class="h-8" />
-            </BaseButton>
-            <span class="truncate text-xl">{{ activity }}</span>
-        </div>
-        <div>
-            <BaseSelect class="font-mono" placeholder="h:mm" :options="PERIOD_SELECT_OPTIONS" :selected="secondsToComplete"
-                @select="secondsToComplete = $event" />
-        </div>
-    </li>
+  <li class="flex flex-col gap-2 p-4">
+    <div class="flex items-center gap-2">
+      <BaseButton :type="BUTTON_TYPE_DANGER">
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="truncate text-xl">{{ activity }}</span>
+    </div>
+    <div>
+      <BaseSelect
+        class="font-mono"
+        placeholder="h:mm"
+        :options="PERIOD_SELECT_OPTIONS"
+        :selected="secondsToComplete"
+        @select="secondsToComplete = $event"
+      />
+    </div>
+  </li>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { TrashIcon } from '@heroicons/vue/24/outline';
-import { PERIOD_SELECT_OPTIONS } from '../constants';
-import { isActivityValid } from '../validators';
-import BaseButton from './BaseButton.vue';
-import BaseSelect from './BaseSelect.vue';
-
-
+import { TrashIcon } from '@heroicons/vue/24/outline'
+import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '../constants'
+import { isActivityValid } from '../validators'
+import BaseButton from './BaseButton.vue'
+import BaseSelect from './BaseSelect.vue'
 
 defineProps({
-    activity: {
-        type: String,
-        required: true,
-        validator: isActivityValid
-    }
+  activity: {
+    type: String,
+    required: true,
+    validator: isActivityValid
+  }
 })
 
 const secondsToComplete = ref(null)
-
-
 </script>
 
 <style lang="scss" scoped></style>
